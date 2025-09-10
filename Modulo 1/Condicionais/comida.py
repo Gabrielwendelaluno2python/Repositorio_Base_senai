2f792a930df8800a52ecba22a9aad85be00c9d95
Gabrielwendelaluno1python <souzagabrielwendeldesouza@gmail.com>
2025-09-10 08:41:29 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/comida.py
@@ -0,0 +1,17 @@
+comida = input('Qual a sua comida favorita?')
+if comida == 'pizza':
+    print('Que incrivel')
+    print('A minha tambem')
+    print('Voce tem bom gosto para culinaria')
+    prato = input('Qual seu sabor de pizza favorito?')
+    if prato == 'frango com catupiry':
+        print('que delicia')
+        print('Me da um pedaço')
+    else:
+        print('toda as pizzas sao boas')
+        print('mas se for de abacaxi')
+        print('saia daqui imediatamente')
+else:
+    print('Voce tem um gosto muito comum')
+    print('Mas voce pode melhorar seus gostos')
+    print('boa sorte ')
