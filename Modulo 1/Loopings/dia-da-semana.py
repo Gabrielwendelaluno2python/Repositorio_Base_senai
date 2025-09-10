023ffc736dd9a62576d399fb92ebeba62ebf0d01
Gabrielwendelaluno1python <souzagabrielwendeldesouza@gmail.com>
2025-09-10 08:44:36 -0300

    Add files via upload

---
+++ Modulo 1/Loopings/dia-da-semana.py
@@ -0,0 +1,36 @@
+n1 =1
+n2 =2
+n3 =3
+n4 =4
+n5 =5
+n6 =6
+n7 =7
+numero = 1000
+while numero != 0:
+    numero =int(input('Digite um numero?'))
+    if numero == n1:
+        print('Domingo') 
+    elif numero == n2:
+        print('Segunda feira')
+    elif numero == n3:
+        print('Terça feira')
+    elif numero == n4:
+        print('Quarta feira')
+    elif numero == n5:
+        print('Quinta feira')
+    elif numero == n6:
+        print('Sexta feira')
+    elif numero == n7:
+        print('Sabado')
+
+    else:
+        print('Numero errado')
+
+print('encerrar programa')
+
+
+
+
+
+
+
