55a61e9b3e046e8f6b3effdf825ae554b78ea808
Gabrielwendelaluno1python <souzagabrielwendeldesouza@gmail.com>
2025-09-10 08:51:56 -0300

    Add files via upload

---
+++ Modulo 1/Loopings/tabuada.py
@@ -0,0 +1,11 @@
+
+print(60 * '-')
+print('TABUADA')
+print(60 * '-')
+tabuada =int(input('|Digite qual tabuada voce queira saber:'))
+contador = 1
+fim = 10
+while contador <= fim:
+    print(f'|{contador} x {tabuada}: {contador * tabuada}')
+    contador = contador + 1
+print(60 * '-')
