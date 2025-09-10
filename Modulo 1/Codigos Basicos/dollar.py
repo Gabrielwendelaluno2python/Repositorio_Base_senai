29021b40c0c3ade71831d9de5199c7dbe076d4e4
Gabrielwendelaluno1python <souzagabrielwendeldesouza@gmail.com>
2025-09-10 08:44:59 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/dollar.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+dollar = float(input('Qual o valor do dollar?'))
+converter = float(input('Convertido para reais?'))
+print (dollar * converter)
+print(f'O valor de ${converter} convertido em reais fica R${dollar * converter}')
