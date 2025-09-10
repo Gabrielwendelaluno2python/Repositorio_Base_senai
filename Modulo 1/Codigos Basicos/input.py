ab1d07586809748596e9f6f92fa9607962d887ef
Gabrielwendelaluno1python <souzagabrielwendeldesouza@gmail.com>
2025-09-10 08:35:57 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/input.py
@@ -0,0 +1,6 @@
+input('Aperte enter para começar.') # input apenas permite digitar no teclado e esperar o usuario pressionara tecla enter
+
+input('digite alguma coisa:') # mostra uma mensagem(string) e espera o usuario digitar no teclado
+
+var = input('digite mais coisa:') # salva o que foi digitado dentro da variavel
+print(var)
