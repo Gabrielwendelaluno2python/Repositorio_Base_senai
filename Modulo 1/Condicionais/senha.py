f92679cade1b3f3613c47a5d39eeb1d7db46d88f
Gabrielwendelaluno1python <souzagabrielwendeldesouza@gmail.com>
2025-09-10 08:51:42 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/senha.py
@@ -0,0 +1,11 @@
+a = '40028922'
+b = 'banana'
+
+
+senha = input('digite a senha?')
+if senha == a:
+    print('senha correta')
+    print('iniciando android')
+else:
+    print('senha incorreta')
+    print('tente novamente')
