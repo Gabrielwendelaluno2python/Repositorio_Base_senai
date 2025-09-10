060fb4190057b5b2b151edf1248eee320514be50
Gabrielwendelaluno1python <souzagabrielwendeldesouza@gmail.com>
2025-09-10 08:43:55 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/cor.py
@@ -0,0 +1,8 @@
+cor = input('Qual a sua cor favorita:')
+if cor == 'preto':
+    print('INCRIVEL')
+    print('voce tem bom gosto')
+    print(':3')
+else:
+    print('que pessimo gosto')
+    print('melhore...')
